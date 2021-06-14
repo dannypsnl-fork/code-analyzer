e54214410fc1089ad96795cd9d328eea0089e9f8
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-06-14 08:37:35 +0800

    dep(data-lib, drracket-tool-lib)

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "code-analyzer")
-(define deps '("base"))
+(define deps '("base" "data-lib" "drracket-tool-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/code-analyzer.scrbl" ())))
 (define pkg-desc "Description Here")
